3fe5ea43d4ed9ea3e12b4e8cd65cbd3c64d195fe
James Smart <jwsmart3@gmail.com>
2023-06-30 21:46:55 +0100

    knights can move apparently
    (it shouldnt be able to do this why can it do this why is that working why is that no stop please)

--- Modules/pieces.py
+++ Modules/pieces.py
@@ -5,6 +5,9 @@ class Piece:
         self._colour = colour
         self._piece = ""
     
+    def get_colour(cls) -> str:
+        return cls._colour
+    
     def set_colour(cls, colour) -> None:
         cls._colour = colour
     
@@ -28,11 +31,9 @@ class Pawn(Piece):
 
         legal_moves = []
 
-        direction = 0
-        if cls._colour == "white":
-            direction = -1
-        else:
-            direction = 1
+        direction = 1
+        if cls._colour == "white" : direction = -1
+
         # straight
         if isinstance(board._rep[cls._pos[1]+direction][cls._pos[0]], Empty):
             legal_moves.append([cls._pos[0],cls._pos[1]+direction])
@@ -53,7 +54,8 @@ class Pawn(Piece):
             if not isinstance(right_square,Empty):
                 legal_moves.append(right_square._pos)
         
-        return legal_moves      
+        return legal_moves
+        
 
 class Rook(Piece):
     def __init__(self, colour="white", pos=[0, 0]) -> None:
@@ -113,6 +115,49 @@ class Knight(Piece):
     def __init__(self, colour="white", pos=[0, 0]) -> None:
         super().__init__(colour, pos)
         self._piece = "n"
+    
+    def get_legal_moves(cls, board):
+        legal_moves = []
+        
+        def horizontal_checker(direction):
+            # -1 = right, 1 = left (ik its confusing lol shut up mosh go away)
+            # checking horizontal to check if the knight can move far in that direction
+            horizontal_limit = 2 if direction == 1 else -5
+            if (cls._pos[0]*direction) >= horizontal_limit:
+                # when both sides are multiplied by -1 the inequality is flipped
+                if cls._pos[1] >= 1:
+                    far_upper = board._rep[cls._pos[1]-1][cls._pos[0]-2*direction]
+                    if isinstance(far_upper, Empty) or far_upper.get_colour() != cls._colour:
+                        # far left upper square is either empty or an enemy piece
+                        legal_moves.append(far_upper._pos)
+                
+                if cls._pos[1] <= 6:
+                    far_lower = board._rep[cls._pos[1]+1*direction][cls._pos[0]-2*direction]
+                    if isinstance(far_lower, Empty) or far_lower.get_colour() != cls._colour:
+                        # far left lower square is either empty or an enemy piece
+                        legal_moves.append(far_lower._pos)
+        
+        
+        def vertical_checker(direction):
+            vertical_limit = 2 if direction == 1 else -5
+            if (cls._pos[1]*direction) >= vertical_limit:
+                # when both sides are multiplied by -1 the inequality is flipped
+                if cls._pos[0] >= 1:
+                    left_square = board._rep[cls._pos[1]-2*direction][cls._pos[0]-1]
+                    if isinstance(left_square, Empty) or left_square.get_colour() != cls._colour:
+                        legal_moves.append(left_square._pos)
+                
+                if cls._pos[0] <= 6:
+                    right_square = board._rep[cls._pos[1]-2*direction][cls._pos[0]+1]
+                    if isinstance(right_square, Empty) or left_square.get_colour() != cls._colour:
+                        legal_moves.append(right_square._pos)
+        
+        horizontal_checker(1)
+        horizontal_checker(-1)
+        vertical_checker(1)
+        vertical_checker(-1)
+        
+        return legal_moves
 
 
 class Bishop(Piece):
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import Modules
 
 class main():
-    def __init__(self, FEN="rnbqkbnr/pppppppp/8/8/3R4/8/PPPPPPPP/1NBQKBNR b Kkq - 0 1") -> None:
+    def __init__(self, FEN="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b Kkq - 0 1") -> None:
         # setting the board
         self.board = Modules.board.Board(FEN)
         print(self.board)
-        print(self.board.get_piece([3,3]).get_legal_moves(self.board))
+        print(self.board.get_piece([0,0]).get_legal_moves(self.board))
     def __str__(self) -> str:
         pass
         
 
 if __name__ == "__main__":
-    game = main()
+    game = main("8/8/8/8/8/8/8/N7 w - - 0 1")
