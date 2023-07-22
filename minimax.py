ecea68641560e9e52f6cf9f8f37b91bbabc3b352
manmosh <kaibritt27@gmail.com>
2023-07-22 14:27:20 -0400

    Update minimax.py

--- minimax.py
+++ minimax.py
@@ -1,7 +1,11 @@
 import evaluation
+import Modules.board
 myEval = evaluation.eval()
 
-def minimax(board,depth,maxPlayer):
+first_board = Modules.board.Board()
+
+
+def minimax(board,depth,alpha,beta,maxPlayer):
     if depth == 0:
         return [myEval.evalPosition(board),board._most_recent_move]
     
@@ -9,20 +13,39 @@ def minimax(board,depth,maxPlayer):
         max_eval = [float('-inf'),1]
         for i in board._legal_moves:
             board.update_position(i)
-            cur_eval = minimax(board,depth-1,False)
+            cur_eval = minimax(board,depth-1,alpha,beta,False)
             max_eval = max(max_eval,cur_eval)
-            max_eval[1] = i
             board.unmove()
-            
+            if cur_eval[0] == max_eval[0]:
+                max_eval[1] = i
+            alpha = max(alpha, cur_eval)
+            if beta <= alpha:
+                break            
+
         return max_eval
     
     if not maxPlayer:
         min_eval = [float('inf'),1]
         for i in board._legal_moves:
             board.update_position(i)
-            cur_eval = minimax(board,depth-1,True)
+            cur_eval = minimax(board,depth-1,alpha,beta,True)
             min_eval = min(min_eval,cur_eval)
             board.unmove()
+            if cur_eval[0] == min_eval[0]:
+                min_eval[1] = i
+            beta = min(beta,cur_eval)
+            if beta <= alpha:
+                break
         return min_eval
+
+turn = True 
+while True:
+
+    first_board.update_position(minimax(first_board,3,[float('-inf'),1],[float('inf'),1],turn)[1])
+    print(first_board)
+    turn = not turn
+
+
+
 
             
