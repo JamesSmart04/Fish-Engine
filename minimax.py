79b055c9bd4ee312e7eb55742eb5f003e11145be
manmosh <kaibritt27@gmail.com>
2023-07-21 20:25:54 -0400

    update minimax
    deleted gross

--- minimax.py
+++ minimax.py
@@ -1,46 +1,6 @@
-import Modules
-import copy
 import evaluation
-import time
-first_board = Modules.board.Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w Kkq - 0 1")
 myEval = evaluation.eval()
 
-# depth1= []
-# for i in first_board._legal_moves:
-#     new_board = copy.deepcopy(first_board)
-#     new_board.update_position(i)
-#     depth1.append(new_board)
-
-# depth2 = []
-# for i in depth1:
-#     for j in i._legal_moves:
-#         new_board = copy.deepcopy(first_board)
-#         new_board.update_position(j)
-#         depth2.append(new_board)
-
-# print(len(depth2))
-
-# get legal moves for board
-# create boards with those positions
-inital = time.time()
-def generateTotalMoves(board, depth):
-    global count
-    if depth == 0:
-        count += 1
-        return 0
-    else:
-        for i in board._legal_moves:
-            board.update_position(i)
-            generateTotalMoves(board, depth-1)
-            board.unmove()
-
-count = 0
-
-# generateTotalMoves(first_board,3)
-# print(time.time()-inital)
-# print(count)
-
-inital = time.time()
 def minimax(board,depth,maxPlayer):
     if depth == 0:
         return [myEval.evalPosition(board),board._most_recent_move]
@@ -64,7 +24,5 @@ def minimax(board,depth,maxPlayer):
             min_eval = min(min_eval,cur_eval)
             board.unmove()
         return min_eval
-            
-print(minimax(first_board,2,True))
-print(time.time()-inital)
+
             
