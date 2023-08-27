6f1bd48cda784cf23683e40d3e0a751d55212e51
KaiBritt <kaibritt27@gmail.com>
2023-08-26 21:56:23 -0400

    Added precaulculated knight move tables

--- Cpp/move_generation.hpp
+++ Cpp/move_generation.hpp
@@ -6,7 +6,7 @@ U64 getFile(int cur_bit) {
     U64 file =  0ULL;
     int file_pointer = cur_bit;
     file |= (1ULL<<cur_bit);
-    while((file_pointer+8)< 63){
+    while((file_pointer+8)<= 63){
         file_pointer += 8;
         file |= (1ULL<<file_pointer);
     }
@@ -18,8 +18,6 @@ U64 getFile(int cur_bit) {
     
     return file;
 }
-
-
 
 U64 getRank(int cur_bit) {
     U64 piece = (1ULL<<cur_bit);
@@ -35,3 +33,38 @@ U64 getRank(int cur_bit) {
 
 }
 
+/***
+ * 
+ * 
+ * Knight !
+ * 
+*/
+U64 knight_right_horiozntal_mask = 4557430888798830399ULL;
+U64 knight_left_horizontal_mask = 18229723555195321596ULL;
+
+extern U64 knight_attacks[64] =  {0ULL}; 
+
+
+// Generating kinght attacks
+    void generateKnightAttackTable () {
+        for (int i = 0; i < sizeof(knight_attacks)/sizeof(knight_attacks[0]); i++){
+            U64 current_attack = 0ULL; // current attack bitboard to be modified below
+            int knight_attack_values[8] =  {i+6,i+15, i-10,i-17,i+10,i+17,i-6,i-15};
+            for (int j = 0; j < sizeof(knight_attack_values)/sizeof(knight_attack_values[0]); j++){   
+                if (knight_attack_values[j] < 0 ||  knight_attack_values[j] > 63){
+                    continue;
+                }
+                else{
+                    current_attack |= ((1ULL<<knight_attack_values[j]));
+            }
+            if ((i%8) > 5){
+                current_attack &= knight_left_horizontal_mask;
+            }
+            if ((i%8) < 2){
+                current_attack &= knight_right_horiozntal_mask;
+            }
+
+            knight_attacks[i] = current_attack;
+        }
+    }
+}
--- Cpp/uci.cpp
+++ Cpp/uci.cpp
@@ -3,13 +3,19 @@
 #include "minimax.hpp"
 #include "move_generation.hpp"
 
+void printBitboard(std::string bitstring){
+    for(int i = 0; i < 8; i++){
+        std::cout << bitstring.substr(i*8,8) << "\n";
+    }
+    return;
+}
 
 int main(){
     std::unordered_map<char,U64> board = readFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     outputBitBoards(board);
-    outputBoard(board);
+    outputBoard(board);                     
     std::cout << exportFEN(board) << "\n";  
-    U64 out = getRank(8);
-    std::cout << std::bitset<std::numeric_limits<U64>::digits>(out).to_string();  
-    return 0;
+    generateKnightAttackTable();
+    printBitboard(std::bitset<std::numeric_limits<U64>::digits>(knight_attacks[63]).to_string());
+   return 0;
 }
